f3e22ab99457e374e48fe6bea8cf3aabbf238256
UktamT <marghel538@gmail.com>
2026-05-24 16:00:27 +0600

    add cors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const FILE_PATH = "./messages.json";
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://github.com/UktamT.github.io"],
     methods: ["GET", "POST"],
   },
 });
